50746f4b66339543120c002b73405c68beaebaa5
xuliang <xuliang@efunong.com>
2019-09-25 15:44:01 +0800

    delete

--- src/main/java/com/liangxu/effectivejava/demo001/StaticFactoryMethod.java
+++
@@ -1,7 +0,0 @@
-package com.liangxu.effectivejava.demo001;
-
-public class StaticFactoryMethod {
-
-    //静态工厂方法
-    //参考Boolean 中 valueOf 方法
-}
--- src/main/java/com/liangxu/effectivejava/demo003/demo03/Elvis.java
+++
@@ -1,7 +0,0 @@
-package com.liangxu.effectivejava.demo003.demo03;
-
-public enum Elvis {
-
-    INSTANCE
-
-}
